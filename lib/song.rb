ae20df1c323cbebdef54848bc89af4bccffc4e35
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-08-01 19:05:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,13 @@
 class Song
   attr_accessor :name, :artist
   @@all=[]
-  def initialize (name)
+  def initialize (name, artist=nil)
     @name=name
+    @artist=artist
     @@all << self
   end
   def self.all
     @@all
   end
+  def 
 end
